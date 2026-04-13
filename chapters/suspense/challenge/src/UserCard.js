1b9623291c34be9aa6d9977d58b8da54cfc31584
Dave Berzack <dave.berzack@nashvillesoftwareschool.com>
2026-04-13 11:55:49 -0400

    confirmed, tweaked suspense

--- chapters/suspense/challenge/src/UserCard.js
+++ chapters/suspense/challenge/src/UserCard.js
@@ -1,4 +1,8 @@
+// Simulate a slow, heavy dependency to show the value of lazy loading
+await new Promise((resolve) => setTimeout(resolve, 3000));
+
 export default function UserCard({ name, title }) {
+  
   return (
     <div className="user-card">
       <h3>{name}</h3>
